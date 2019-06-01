1484bf5972a63b2038e1275c124852a733eb523d
Paweł Nowak <pawelpnowak@outlook.com>
2019-06-01 12:56:37 +0200

    add components lib

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from '@dpf-workspace/data';
 
 @Component({
   selector: 'dpf-workspace-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todos';
+
+  todo: Todo;
+
+  constructor() {
+    this.todo = {
+      title: 'Ala ma kota'
+    }
+  }
 }
